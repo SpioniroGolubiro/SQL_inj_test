dac5ea1f0a80f99e2d509c0433f4b9f420b7d0be
SpioniroGolubiro <emailspazzatura27@gmail.com>
2025-04-28 17:09:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const db = require('./db');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
